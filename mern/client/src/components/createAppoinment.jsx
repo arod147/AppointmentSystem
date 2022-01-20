95581f43b597dd69743b9312e4268b82834f4d56
arod147 <code.alex.rodriguez@outlook.com>
2022-01-19 20:26:58 -0500

    Created schedule appointment jsx

--- mern/client/src/components/createAppoinment.jsx
+++ mern/client/src/components/createAppoinment.jsx
@@ -1,47 +1,87 @@
 import { Button, Form, FormControl, FormGroup, FormLabel, FormSelect, Row, Col, ButtonGroup } from "react-bootstrap"
-import { useEffect } from 'react'
+import { useState } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 //First and last name
 //Service type
 //Date and time
 //Email address for comfirmation
-const CreateAppointment = () => {
-    useEffect(() => {
 
-    }, [])
+    const AvailableEmployees = (props) => (
+        <option value={props.name}>{props.name}</option>
+    )
+
+    const AvailableTimes = (props) => (
+        <Button value={props.time} className="m-1" variant="primary">{props.time}</Button>
+    )
+
+const CreateAppointment = () => {
+    const [value, onChange] = useState(new Date())
+    const employeeList = ['Alex', 'Mark']
+    const [employeeInfo, updateEmployeeInfo] = useState(
+        {
+            firstName: 'Alex',
+            lastName: 'Rodriguez',
+            availableDays: [
+                {
+                    date: value,
+                    times: ['1:00pm', '2:00pm']
+                }
+            ]
+        }
+    )
+
+    const emps = employeeList.map((emp, index) => {
+            return <AvailableEmployees key={index} name={emp}/>
+        })
+    //Show availbale days 
+    function disableTiles({date}) {
+        return employeeInfo.availableDays.find(day => day.date.getDate() !== date.getDate())
+    }
+
+    const times = employeeInfo.availableDays.map((day, index) => {
+        return day.times.map(time => {
+            return <AvailableTimes key={index} time={time}/>
+        }) 
+        })
+
     return (
         <div>
             <Form>
+            <FormGroup className="mb-4">
+                    <FormLabel>Services</FormLabel>
+                    <FormSelect aria-label="Default select example">
+                        <option>Please Choose A Service</option>
+                        <option value="Service 1">Service 1</option>
+                    </FormSelect>
+                </FormGroup>
                 <FormGroup className="mb-4">
                     <FormLabel>Staff</FormLabel>
                     <FormSelect aria-label="Default select example">
                         <option>Choose Staff employee</option>
-                        <option value="Staff 1">Staff 1</option>
-                        <option value="Staff 2">Staff 2</option>
-                        <option value="Staff 3">Staff 3</option>
+                        {emps}
                     </FormSelect>
                 </FormGroup>
                 <Row>
                     <FormGroup className="mb-4" as={Col}>
                         <FormLabel>Date</FormLabel>
-                        
+                        <Calendar
+                            minDetail="month"
+                            nextLabel={null}
+                            next2Label={null}
+                            prevLabel={null}
+                            prev2Label={null}
+                            tileDisabled={disableTiles}
+                            showNeighboringMonth={false}
+                            onChange={onChange}
+                            value={value}
+                        />
                     </FormGroup>
                     <FormGroup className="mb-4" as={Col}>
                         <FormLabel>Time</FormLabel>
                         <ButtonGroup aria-label="Basic example">
                             <Col>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
-                                <Button className="m-1" variant="primary">1:00pm</Button>
+                                {times}
                             </Col>
                         </ButtonGroup>
                     </FormGroup>
